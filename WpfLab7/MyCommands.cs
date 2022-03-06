d05d606ee3b34397648331a75bb0ecf7ee9f576e
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-03-06 15:41:34 +0300

    Добавьте файлы проекта.

---
+++ WpfLab7/MyCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfLab7
+{
+    internal class MyCommands
+    {
+        public static RoutedCommand Open { get; set; }
+        public static RoutedUICommand Help { get; set; }
+        public static RoutedUICommand Contacts { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.O, ModifierKeys.Control, "Ctrl+O"));
+            Open = new RoutedCommand("Open", typeof(MyCommands), inputs);
+
+            InputGestureCollection inputs1 = new InputGestureCollection();
+            inputs1.Add(new KeyGesture(Key.H, ModifierKeys.Control, "Ctrl+H"));
+            Help = new RoutedUICommand("Помощь", "Help", typeof(MyCommands), inputs1);
+            
+            Contacts = new RoutedUICommand("Контакты", "Contacts", typeof(MyCommands));
+        }
+    }
+}
